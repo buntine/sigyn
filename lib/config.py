d2af41b7112045df77cd644e7de852970cceeb6e
Andy Buntine <bunts@hhd.com.au>
2012-10-08 16:27:34 +1100

    Updated conf model

--- lib/config.py
+++ lib/config.py
@@ -5,12 +5,17 @@ class Conf:
         self.conf = ConfigParser.ConfigParser()    
         self.section = None
 
-    def read(self, section):
+    def read(self, section=None):
+        '''Reads the config file and optionally sets the section'''
         path = os.path.join(os.path.dirname(__file__), "..", "config", "config.cfg")
         self.conf.read(path)
+        self.set_section(section)
 
-        if self.conf.has_section(section):
+    def set_section(self, section):
+        if section is not None and self.has_section(section):
             self.section = section
+        else:
+             self.section = None
 
     def get(self, option, default=None):
         if self.section is not None and self.has_option(option):
@@ -18,11 +23,17 @@ class Conf:
         else:
             return default
 
-    def add(self, details):
+    def sites(self):
         pass
 
-    def remove(self, section):
+    def add_site(self, details):
+        pass
+
+    def remove_site(self, section):
         pass
 
     def has_option(self, option):
         return self.conf.has_option(self.section, option)
+
+    def has_section(self, section):
+        return self.conf.has_section(section)

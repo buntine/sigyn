27dcb2fed2f394beb2cdcc707da922aedf82a51f
Andy Buntine <bunts@hhd.com.au>
2012-10-16 17:12:44 +1100

    Setup basic file logging for each site

--- lib/sigyn.py
+++ lib/sigyn.py
@@ -1,13 +1,15 @@
 from lib import website, config
+import logging
+import logging.config
 
 class Sigyn:
     def __init__(self):
+        logging.config.fileConfig("config/logging.cfg")
+
         self.conf = config.Conf(do_read=True)
         self.sites = self.conf.sites()
 
     def check(self):
-        print "Checking..."
-
         for site in self.sites:
             self.conf.set_section(site)
 
--- lib/website.py
+++ lib/website.py
@@ -1,7 +1,16 @@
+import logging
+
 class Website:
     def __init__(self, domain, config):
         self.domain = domain
         self.config = config
 
+        handler = logging.FileHandler('logs/' + domain + ".log")
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+
+        self.logger = logging.getLogger(domain)
+        self.logger.addHandler(handler)
+
     def check(self):
-        print "Checking " + self.domain
+        self.logger.info("Checking " + self.domain)
